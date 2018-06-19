da52d1b9b8a33e02159e7195cd8d16ab8ca0bf06
Alex Kaszynski <akascap@gmail.com>
2018-06-19 15:21:33 +0200

    added windows batch

---
+++ windows_build.bat
@@ -0,0 +1,27 @@
+@echo off
+
+
+REM For Python 2.7 #####################################################
+call:build_wheel "C:\Python27\python.exe"
+call:build_wheel "C:\Program Files\Python35\python.exe"
+call:build_wheel python
+
+::--------------------------------------------------------
+::-- Build and test a python wheel
+::--------------------------------------------------------
+:build_wheel
+
+pip=%~1 -m pip
+%pip% install pip --upgrade pip --user
+%pip% install wheel --user
+%pip% install setuptools --user --upgrade
+%pip% install numpy --user
+%pip% install vtk --user
+
+%~1 %~dp0setup.py bdist_wheel
+
+rem for testing
+%pip% uninstall pymeshfix
+%~1 %~dp0setup.py install --user
+%~1 %~dp0pymeshfix\examples\fix.py
+goto:eof
