66bd24958427f0c78715f3fe34d3dcd74c252dcc
Alex Kaszynski <akascap@gmail.com>
2020-07-01 21:43:45 +0200

    fixed setup classifiers and disabled upload to testing pypi

--- setup.py
+++ setup.py
@@ -78,8 +78,8 @@ setup(
                  'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7'
-                 'Programming Language :: Python :: 3.7'
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8'
     ],
     url='https://github.com/pyvista/pymeshfix',
 
