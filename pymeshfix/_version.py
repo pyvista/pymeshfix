2ef77058afeec6b34795357602ba4df09e177f48
Eric Larson <larson.eric.d@gmail.com>
2025-12-12 12:16:56 -0500

    BUG: Fix bug with release

--- pymeshfix/_version.py
+++ pymeshfix/_version.py
@@ -3,9 +3,9 @@
 On the ``master`` branch, use 'dev0' to denote a development version.
 For example:
 
-version_info = 0, 27, 'dev0'
+version_info = 0, 18, "dev0"
 
 """
 
-version_info = 0, 18, "dev0"
+version_info = 0, 17, 2
 __version__ = ".".join(map(str, version_info))
