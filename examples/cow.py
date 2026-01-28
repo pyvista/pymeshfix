c19af037bced7c7d4099563527384a7ce7e59c99
Alex Kaszynski <akascap@gmail.com>
2026-01-28 11:41:59 -0700

    make pc

--- examples/cow.py
+++ examples/cow.py
@@ -18,20 +18,20 @@ cow = examples.download_cow()
 
 # Add holes and cast to triangulated PolyData
 cow["random"] = np.random.rand(cow.n_cells)
-holy_cow = cow.threshold(0.9, invert=True).extract_geometry().triangulate()
-print(holy_cow)
+cow_w_holes = cow.threshold(0.9, invert=True).extract_geometry().triangulate()
+print(cow_w_holes)
 
 ################################################################################
 
 # A nice camera location of the cow
 cpos = [(6.56, 8.73, 22.03), (0.77, -0.44, 0.0), (-0.13, 0.93, -0.35)]
 
-meshfix = mf.MeshFix(holy_cow)
+meshfix = mf.MeshFix(cow_w_holes)
 holes = meshfix.extract_holes()
 
 # Render the mesh and outline the holes
 p = pv.Plotter()
-p.add_mesh(holy_cow, color=True, smooth_shading=True, split_sharp_edges=True)
+p.add_mesh(cow_w_holes, color=True, smooth_shading=True, split_sharp_edges=True)
 p.add_mesh(holes, color="r", line_width=8)
 p.camera_position = cpos
 p.camera.zoom(1.5)
